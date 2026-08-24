74ba8aab4965a9a964be0204f2a7aaa2c5268046
shepelevep <shepelevep@answer-42.ru>
2026-08-24 14:22:00 +0300

    add trackes fetching

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,45 @@
+import { useState, useEffect } from "react";
 import "./App.css";
-import { useState } from "react";
+import  { API_KEY } from '../api-key.js';
 
-const tracks = [
-  {
-    id: 1,
-    title: "music track 1",
-    url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
-  },
-  {
-    id: 2,
-    title: "instrumental",
-    url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
-  },
-  {
-    id: 3,
-    title: "music track 2",
-    url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
-    isSelected: true,
-  },
-  {
-    id: 4,
-    title: "instrumental 2",
-    url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
-  },
-];
+// const tracks = [
+//   {
+//     id: 1,
+//     title: "music track 1",
+//     url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
+//   },
+//   {
+//     id: 2,
+//     title: "instrumental",
+//     url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
+//   },
+//   {
+//     id: 3,
+//     title: "music track 2",
+//     url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack.mp3",
+//     isSelected: true,
+//   },
+//   {
+//     id: 4,
+//     title: "instrumental 2",
+//     url: "https://musicfun.it-incubator.app/api/samurai-way-soundtrack-instrumental.mp3",
+//   },
+// ];
 
 function App() {
   const [selectedTrackId, setSelectedTrackId] = useState<number | null>(null);
+  const [tracks, setTracks] = useState([]);
+
+  useEffect(
+    () => {
+      fetch('https://musicfun.it-incubator.app/api/1.0/playlists/tracks', {
+        headers: {
+          'api-key' : API_KEY
+        }
+      }).then(res => res.json())
+      .then(json => setTracks(json.data))
+    }, []
+  );
 
   if (tracks === null) {
     return (
@@ -62,9 +75,9 @@ function App() {
             border: selectedTrackId === track.id ? "2px solid blue" : "none",
           }}
         >
-          <h3 onClick={() => setSelectedTrackId(track.id)}>{track.title}</h3>
+          <h3 onClick={() => setSelectedTrackId(track.id)}>{track.attributes.title}</h3>
           <audio controls>
-            <source src={track.url} type="audio/mpeg" />
+            <source src={track.attributes.attachments[0].url} type="audio/mpeg" />
           </audio>
         </div>
       ))}
